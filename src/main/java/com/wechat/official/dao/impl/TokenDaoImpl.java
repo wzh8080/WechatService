ff8faecbbc5bbf8b5a02cfd3c879280ec02dfd6b
PC <wzh8080@foxmail.com>
2024-05-15 21:31:51 +0800

    2

--- src/main/java/com/wechat/official/dao/impl/TokenDaoImpl.java
+++ src/main/java/com/wechat/official/dao/impl/TokenDaoImpl.java
@@ -1,10 +1,11 @@
-package com.wechat.official.dao;
+package com.wechat.official.dao.impl;
 
+import com.wechat.official.dao.TokenDao;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TokenDaoImpl implements TokenDao{
+public class TokenDaoImpl implements TokenDao {
     /**
      * 若 token 过期，返回null ， 不过期则取库中 token
      * @return
@@ -12,7 +13,7 @@ public class TokenDaoImpl implements TokenDao{
     @Override
     @Nullable
     public String getToken() {
-
+        
         return null;
     }
 }
